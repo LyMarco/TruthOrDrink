5636376a9a43098f1b0c9f4454275125ebb25752
Marco Ly <ly_marco@yahoo.ca>
2024-09-10 02:43:20 -0400

    Added sound effects and button disabling
    Had to add in the old horrible onClickListeners for this easy way to disable buttons. Would like the elegant way if possible

--- app/src/main/java/com/example/truthordrink/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/truthordrink/ui/dashboard/DashboardFragment.java
@@ -3,6 +3,7 @@ package com.example.truthordrink.ui.dashboard;
 import android.annotation.SuppressLint;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,7 +25,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class DashboardFragment extends Fragment implements View.OnClickListener {
+public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
@@ -36,12 +37,14 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     private final List<String> pickDeckList = Arrays.asList("OTR", "HR", "ED", "LC");
     private int pickDeck = 0;
 
-
     private Random rand = new Random();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         // DashboardViewModel lines were here
+
+        final MediaPlayer press = MediaPlayer.create(getActivity(), R.raw.button);
+        final MediaPlayer flip = MediaPlayer.create(getActivity(), R.raw.flipping);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -69,46 +72,67 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         ImageButton lcButton = binding.imageButtonLC;
         Button drawButton = binding.drawButton;
 
-        otrButton.setOnClickListener(this);
-        hrButton.setOnClickListener(this);
-        edButton.setOnClickListener(this);
-        lcButton.setOnClickListener(this);
+//        otrButton.setOnClickListener(this);
+//        hrButton.setOnClickListener(this);
+//        edButton.setOnClickListener(this);
+//        lcButton.setOnClickListener(this);
 
-//        otrButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View otrButton) {
-//                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
-//                pickDeck = 1;
-//            }
-//        });
-//
-//        hrButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View hrButton) {
-//                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
-//                pickDeck = 2;
-//            }
-//        });
-//
-//        edButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View edButton) {
-//                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
-//                pickDeck = 3;
-//            }
-//        });
-//
-//        lcButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View lcButton) {
-//                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
-//                pickDeck = 4;
-//            }
-//        });
+        otrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View otrButton) {
+                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
+                pickDeck = 1;
+                press.start();
+                otrButton.setEnabled(false);
+                hrButton.setEnabled(true);
+                edButton.setEnabled(true);
+                lcButton.setEnabled(true);
+            }
+        });
+
+        hrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View hrButton) {
+                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
+                pickDeck = 2;
+                press.start();
+                otrButton.setEnabled(true);
+                hrButton.setEnabled(false);
+                edButton.setEnabled(true);
+                lcButton.setEnabled(true);
+            }
+        });
+
+        edButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View edButton) {
+                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
+                pickDeck = 3;
+                press.start();
+                otrButton.setEnabled(true);
+                hrButton.setEnabled(true);
+                edButton.setEnabled(false);
+                lcButton.setEnabled(true);
+            }
+        });
+
+        lcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View lcButton) {
+                imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.deck_example));
+                pickDeck = 4;
+                press.start();
+                otrButton.setEnabled(true);
+                hrButton.setEnabled(true);
+                edButton.setEnabled(true);
+                lcButton.setEnabled(false);
+            }
+        });
 
         drawButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View drawButton) {
+                flip.start();
                 switch (pickDeck) {
                     case 1:
                         imageView2.setImageDrawable(ContextCompat.getDrawable(requireContext(), OTRList.get(rand.nextInt(OTRList.size()))));
@@ -132,30 +156,25 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         return root;
     }
 
-    public void setDisabled(ImageView imageButton) {
-        final ColorMatrix grayscaleMatrix = new ColorMatrix();
-        grayscaleMatrix.setSaturation(0);
-        imageButton.setColorFilter(new ColorMatrixColorFilter(grayscaleMatrix));
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.imageButtonOTR:
-                pickDeck = 1;
-                break;
-            case R.id.imageButtonHR:
-                pickDeck = 2;
-                break;
-            case R.id.imageButtonED:
-                pickDeck = 3;
-                break;
-            case R.id.imageButtonLC:
-                pickDeck = 4;
-                break;
-        }
-    }
+//    @SuppressLint("NonConstantResourceId")
+//    @Override
+//    public void onClick(View view) {
+//        press.start();
+//        switch (view.getId()) {
+//            case R.id.imageButtonOTR:
+//                pickDeck = 1;
+//                break;
+//            case R.id.imageButtonHR:
+//                pickDeck = 2;
+//                break;
+//            case R.id.imageButtonED:
+//                pickDeck = 3;
+//                break;
+//            case R.id.imageButtonLC:
+//                pickDeck = 4;
+//                break;
+//        }
+//    }
 
     @Override
     public void onDestroyView() {
